ac05b1994d326b3687438c27457761d35370fa29
sambit-innofied <sambit.sarkar@innofied.com>
2025-10-06 11:12:21 +0530

    working on frontend card (date, tags)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,9 +11,33 @@
 
     <div class="container ps-5 pe-5">
         <div class="row">
-            <div class="col-12 p-2 text-center mt-4 mb-4 border-bottom-black" >
+            <div class="col-12 p-2 text-center mt-4 mb-4 border-bottom-black">
                 <h1>Welcome to my Blog</h1>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, id!</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-8 col-12">
+                @foreach ($blogs as $blog)
+                    <div class="row mt-4 mb-4 blog-card border rounded">
+                        <div class="col-lg-4 col-12 p-0 m-0">
+                            <img class="rounded w-100 h-100" src="{{ $blog['image_url_potrait'] }}" alt="">
+                        </div>
+                        <div class="col-lg-8 col-12">
+                            <div class="row h-100 pt-4 align-item-center">
+                                <div class="col-12 mx-auto">
+                                    <small class="text-muted fs-8">{{$blog['date']}}</small>
+                                    <br>
+                                    @foreach (explode(",", $blog['tags']) as $tag)
+                                        <span class="text primary fw-bolder gs-6 pe-1">{{$tag}}</span>
+                                    @endforeach
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                @endforeach
             </div>
         </div>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,22 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', [
+        "title" => "My Blog",
+
+        "blogs" => [
+            [
+                "url" => "/",
+                "is_trending" => true,
+                "author" => "Sambit Sarkar",
+                "author_image_url" => "https://avatars.githubusercontent.com/u/87202351?v=4",
+                "image_url_potrait" => "https://fastly.picsum.photos/id/299/300/350.jpg?hmac=w9b4w232XL4IUBN3jok3KfxDBkntCT5NdUPUpmnooQY",
+                "image_url_landscape" => "https://fastly.picsum.photos/id/319/360/160.jpg?hmac=NLWFlVNpsj8z59NQlo7KqTjGlQP2Ab0emA-xqHQ3YAA",
+                "title" => "Laravel Websockets | Chat Application",
+                "date" => "October 06, 2022",
+                "description" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quasi dolor officia, architecto sapiente optio earum.",
+                "tags" => "newyork,building,street"
+            ]
+        ]
+    ]);
 });
